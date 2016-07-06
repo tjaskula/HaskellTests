eb235572026c93d31e772a61e0dab95805e4b803
Tomasz Jaskula <thomasv1000@hotmail.fr>
2016-07-06 14:02:26 +0200

    Fixing code sample

--- Test.hs
+++ Test.hs
@@ -1,13 +1,14 @@
 import System.IO
 import Control.Monad
+import Data.List
 import Data.List.Split
 
 main :: IO ()
 main = do
-
+    
     let n = 5
     let temps = "1 -4 5 2 -3"
-
+    
     if n == 0 
         then putStrLn "0"
     else do
@@ -16,10 +17,11 @@ main = do
         let positives = filter (>0) numbers
         let negatives = filter (<0) numbers
         
-        let closerToZero a [] = min a
-            closerToZero [] b = max b
-            closerToZero a b = if min a <= max(abs b) then min a else max b
+        let closerToZero a [] = minimum a
+            closerToZero [] b = maximum b
+            closerToZero a b = if minimum a <= abs(maximum b) then minimum a else maximum b
         
         let result = closerToZero positives negatives
-        putStrLn result
+        putStrLn (show result)
+        
     return ()
